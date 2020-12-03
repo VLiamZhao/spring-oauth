617080e82a44e9a70d25c18a271cdef82f7d8b5d
Can Zhao <canzhao216@gmail.com>
2020-12-02 21:41:22 -0500

    test

---
+++ src/test/java/io/javabrains/oauthfacebooklogin/OauthFacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.oauthfacebooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthFacebookLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
